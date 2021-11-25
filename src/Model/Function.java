b4eaba3f80831fda3c074286f8dcf42b1c998d36
ADAM Pauline <pauline92@hotmail.fr>
2021-11-26 00:15:52 +0100

    added tests

--- src/Model/Function.java
+++ src/Model/Function.java
@@ -5,6 +5,10 @@ public class Function {
     private double z;
     private Complex c;
 
+    public Function(Complex c){
+        this.c = c;
+    }
+
     public Complex img(Complex z){
         return z.add(c);
     }
