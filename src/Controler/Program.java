1e4b62b6168c2b05796f8c328a1d6d8e7657a026
ADAM Pauline <pauline92@hotmail.fr>
2021-12-04 18:29:28 +0100

    Solved double precision in the program

--- src/Controler/Program.java
+++ src/Controler/Program.java
@@ -29,6 +29,38 @@ public class Program {
         return ite;
     }
 
+    public double chercheMult(double pas){
+        String val = String.valueOf(pas);
+        System.out.println(val.length());
+        System.out.println(val.length()+"+"+val+"+"+val.length());
+        if (val.length() < 3){
+            return 1;
+        } else {
+            boolean virgule = false;
+            double compt = 0.0;
+            int i=0;
+            while (i != val.length()){
+                if (val.charAt(i) == '.'){
+                    if (val.length() == i + 2){
+                        if (val.charAt(i+1) == '0'){
+                            return 1;
+                        }
+                    }
+                    virgule = true;
+                }
+                else if (virgule){
+                    compt++;
+                }
+                i++;
+            }
+            if (compt == 0){
+                return 1;
+            } else {
+                return Math.pow(10.0,compt);
+            }
+        }
+    }
+
 
     public void createFractale(double[] rect_s, double pas, String fic) throws IOException {
         double[][] rect = {{rect_s[0],rect_s[1]}, {rect_s[2],rect_s[3]}};
@@ -36,16 +68,21 @@ public class Program {
         double h = (rect[1][1] - rect[1][0])/pas;
         int[][] tab_ind = new int[(int)w][(int)h];
         int compi=0;
-
-        for (double r = rect[0][0]; r < rect[0][1];r+=pas){
-            int compj=0;
-            for (double i = rect[1][0]; i < rect[1][1];i+=pas){
-                Complex c = new Complex.Builder(r,i).build();
+        double mult = chercheMult(pas);
+        System.out.println(mult);
+        double rl = rect[0][0];
+        while (rl < rect[0][1]){
+            int compj = 0;
+            double i = rect[1][0];
+            while( i < rect[1][1]){
+                Complex c = new Complex.Builder(rl,i).build();
                 int ind = this.divergenceIndex(c);
                 tab_ind[compi][compj] = ind;
                 compj++;
+                i = Math.round( (i + pas)*mult ) / mult;
             }
             compi++;
+            rl = Math.round( (rl + pas)*mult ) / mult;
         }
 
         var img=new BufferedImage((int)w, (int)h, BufferedImage.TYPE_INT_RGB);
